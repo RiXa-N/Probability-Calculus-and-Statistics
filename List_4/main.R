c07b95d5c77c1d6fb9bc2019e68ff9084d009257
RiXa-N <nikonenkovladyslav0@gmail.com>
2025-11-25 11:57:15 +0100

    comments add

--- List_4/main.R
+++ List_4/main.R
@@ -5,10 +5,10 @@ set.seed(12345)
 
 u <- runif(5000, min=0, max=1)
 
-png("hist_uniform.png", width = 800, height = 600)
-hist(u, breaks=30, col="red", prob=TRUE, main="Histogram - U[0,1], n=5000", xlab="Wartość")
+png("hist_uniform.png", width = 800, height = 600)     # zapis histogramu do pliku PNG
+hist(u, breaks=30, col="red", prob=TRUE,main="Histogram - U[0,1], n=5000", xlab="Wartość") # prob=TRUE → histogram gęstości
 
-dens_u <- density(u)
+dens_u <- density(u) # estymator jądrowy gęstości
 
 png("kde_uniform.png", width = 800, height = 600)
 plot(dens_u, col="red", main="Estymator jądrowy - U[0,1], n=5000", xlab="Wartość")
@@ -32,18 +32,15 @@ cat("Zad.2 \n")
 # a) i)
 set.seed(123)
 
-# 600 realizacji z U[0,1]
-u <- runif(600)
-
-# Rzuty kostką: X = ceil(6u)
-rzuty <- ceiling(6 * u)
+u <- runif(600)                     # 600 realizacji U[0,1]
+rzuty <- ceiling(6 * u)            # X = ceil(6u): generowanie liczb 1–6
 
 head(rzuty, 5)
 cat("\n")
 
 # a) ii)
-srednia <- mean(rzuty)
-wariancja <- var(rzuty)
+srednia <- mean(rzuty)  # średnia z próby
+wariancja <- var(rzuty) # wariancja z próby
 
 srednia
 wariancja
@@ -57,7 +54,7 @@ cat("\n")
 # a) iv)
 ramka <- as.data.frame(czestosci)
 ramka
-var_czestosci <- var(ramka$Freq)
+var_czestosci <- var(ramka$Freq)   # wariancja liczności kategorii
 var_czestosci
 cat("\n")
 
@@ -67,7 +64,6 @@ set.seed(123)
 rzuty_sample <- sample(1:6, size = 600, replace = TRUE)
 
 head(rzuty_sample)
-
 table(rzuty_sample)
 mean(rzuty_sample)
 var(rzuty_sample)
@@ -88,7 +84,8 @@ p_sorted <- p[ord]
 # prawdopodobieństwa skumulowane
 s <- cumsum(p_sorted)
 
-u <- runif(1000)
+u <- runif(1000)                   # 1000 realizacji U[0,1]
+X <- numeric(1000)                 # wektor wyników
 
 # generujemy X według reguły:
 #    X = k_i   gdy   s_(i-1) <= u < s_i
@@ -108,6 +105,7 @@ table(X)
 
 # Zad.4
 cat("\nZad.4\n")
+
 # i)
 set.seed(123)
 
@@ -117,8 +115,8 @@ p <- 0.3
 binom_sample <- numeric(100)
 
 for (i in 1:100) {
-  u <- runif(n)
-  sukcesy <- sum(u < p)
+  u <- runif(n)                    # 10 prób jednorazowych
+  sukcesy <- sum(u < p)            # sukces gdy U < p
   binom_sample[i] <- sukcesy
 }
 
@@ -128,19 +126,16 @@ cat("\n")
 
 # ii)
 set.seed(123)
-
 p <- 0.4
 geom_sample <- numeric(50)
 
 for (i in 1:50) {
   licznik <- 1
   u <- runif(1)
-
-  while (u >= p) {
+  while (u >= p) {                 # powtarzamy aż trafimy sukces
     licznik <- licznik + 1
     u <- runif(1)
   }
-
   geom_sample[i] <- licznik
 }
 
@@ -153,7 +148,7 @@ cat("\nZad.5\n")
 # a) i)
 losuj_odwrotnie <- function(n) {
   u <- runif(n)
-  x <- 2 * sqrt(u) # odwrotna dystrybuanta
+  x <- 2 * sqrt(u)                 # odwrotna dystrybuanta F^{-1}(u)
   return(x)
 }
 
@@ -161,6 +156,7 @@ losuj_odwrotnie <- function(n) {
 set.seed(123)
 
 realizacje_odwrotnie <- losuj_odwrotnie(200)
+
 png("hist_wisualise.png", width = 800, height = 600)
 hist(realizacje_odwrotnie, breaks=20, col="blue", main="Histogram - metoda odwracania dystrybuanty", xlab="x")
 
@@ -169,9 +165,9 @@ losuj_przyjecie_odrzucenie <- function(n) {
   x <- numeric(n)
   i <- 1
   while(i <= n) {
-    y <- runif(1, 0, 2)
+    y <- runif(1, 0, 2)            # kandydat z rozkładu pomocniczego
     u <- runif(1)
-    if(u <= (0.5*y) / 1) {    # g(y)/ (M*f(y)) = 0.5*y / 1 = 0.5*y
+    if(u <= (0.5*y)) {             # reguła przyjęcia: g(y)/(M*h(y)) = 0.5*y
       x[i] <- y
       i <- i + 1
     }
@@ -183,5 +179,6 @@ losuj_przyjecie_odrzucenie <- function(n) {
 set.seed(123)
 
 realizacje_przyjecie <- losuj_przyjecie_odrzucenie(200)
+
 png("hist_wisualise_2.png", width = 800, height = 600)
 hist(realizacje_przyjecie, breaks=20, col="green", main="Histogram - metoda przyjęcia i odrzucenia", xlab="x")
